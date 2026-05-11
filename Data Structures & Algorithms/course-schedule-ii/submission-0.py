6351e0b6f9d7ddf90bcbb4edcd5ba325e35f3551
Jade Raymond <jade.cf.raymond@gmail.com>
2026-05-10 23:11:58 -0400

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        prereqs_left = [set() for _ in range(numCourses)]
+        follow_ups = [[] for _ in range(numCourses)]
+        for course, preq in prerequisites:
+            prereqs_left[course].add(preq)
+            follow_ups[preq].append(course)
+        
+        taken = set()
+        q = deque()
+        for course, prereqs in enumerate(prereqs_left):
+            if len(prereqs) == 0:
+                q.append(course)
+                taken.add(course)
+
+        courses = [] 
+        while q:
+            course = q.popleft()
+            courses.append(course)
+
+            for next_course in follow_ups[course]:
+                if next_course in taken:
+                    continue
+                
+                for prereq in tuple(prereqs_left[next_course]):
+                    if prereq in taken:
+                        prereqs_left[next_course].remove(prereq)
+                if len(prereqs_left[next_course]) == 0:
+                    q.append(next_course)
+                    taken.add(next_course)
+        
+        return courses if len(taken) == numCourses else []
+
+
