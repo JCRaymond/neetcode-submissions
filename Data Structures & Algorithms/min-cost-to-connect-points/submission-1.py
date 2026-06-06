8811782f03e2db4a05afe81d00b2aad42c7bcc54
Jade Raymond <jade.cf.raymond@gmail.com>
2026-06-05 23:06:07 -0400

    Add: min-cost-to-connect-points - submission-1

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-1.py
@@ -0,0 +1,39 @@
+class Solution:
+    inf = float('inf')
+
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        visited = [False]*n
+        dist = [Solution.inf]*n
+
+        min_cost = 0
+
+        dist[0] = 0
+        curr = 0
+        for _ in range(n-1):
+            visited[curr] = True
+            cx, cy = points[curr]
+
+            next_point = None
+            next_dist = Solution.inf
+            for neighbor in range(n):
+                if visited[neighbor]:
+                    continue
+                
+                nx, ny = points[neighbor]
+                n_dist = abs(nx - cx) + abs(ny - cy)
+
+                if n_dist < dist[neighbor]:
+                    dist[neighbor] = n_dist
+                
+                if dist[neighbor] < next_dist:
+                    next_point = neighbor
+                    next_dist = dist[neighbor]
+            
+            curr = next_point
+            min_cost += next_dist
+
+        return min_cost 
+
+
+
